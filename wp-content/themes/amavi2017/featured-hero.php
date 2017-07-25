8d3a4e239c242468682bfae37a5530fb9e738b53
Svanhild Egge <svanhildve@gmail.com>
2017-07-25 22:57:26 +0200

    Started the big job of fixing the featured hero

--- wp-content/themes/amavi2017/featured-hero.php
+++ wp-content/themes/amavi2017/featured-hero.php
@@ -1,5 +1,11 @@
 <div class="featured-hero row large-wrapper">
 
+	<div class="featured-hero__thumbnail col-s-12 col-ml-offset-5 col-ml-7 col-l-offset-4 col-l-8">
+		<div class="featured-hero__thumbnail-wrap">
+			<a href="<?php the_permalink(); ?>"><div class="featured-hero__thumbnail-image" style="background-image:url('<?php echo wp_get_attachment_url( get_post_thumbnail_id() );?>');"></div></a>
+		</div>
+	</div>
+
 	<div class="featured-hero__teaser col-s-12 col-pm-9 col-m-7 col-ml-5 col-l-5">
 
 		<div class="meta">
@@ -26,9 +32,6 @@
 
 	</div>
 
-	<div class="featured-hero__thumbnail col-s-12 col-ml-offset-5 col-ml-7 col-l-offset-4 col-l-8">
-		<a href="<?php the_permalink(); ?>"><div class="featured-hero__thumbnail-image" style="background-image:url('<?php echo wp_get_attachment_url( get_post_thumbnail_id() );?>');">
-		</div></a>
-	</div>
+	
 
 </div>
