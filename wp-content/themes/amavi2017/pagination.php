e9a3ddb8e1312fa6b36fec5bf19a558a385c23bf
Svanhild Egge <svanhildve@gmail.com>
2017-09-24 20:47:31 +0200

    Finally fixed pagination! <3 <3 It is totes working now

--- wp-content/themes/amavi2017/pagination.php
+++ wp-content/themes/amavi2017/pagination.php
@@ -2,18 +2,19 @@
 
 				<div class="pagination__body col-s-12">
 
-					<div class="pagination__next"><?php previous_posts_link( '< previous' ); ?></div>
+					<div class="pagination__previous"><?php previous_posts_link( '<span class="pagination__previous-link">previous</span>' ); ?></div>
 					<div class="pagination__current">
 
 						<?php 
 
 							$paged = (get_query_var('paged')) ? get_query_var('paged') : 1; 
-							echo 'page '. $paged.' of '.$wp_query->max_num_pages; 
+							echo 'page '. $paged.' of '.$wp_query->max_num_pages;
 
 						?>
 						
 					</div>
-					<div class="pagination__previous"><?php next_posts_link( 'next' ); ?></div>
+
+					<div class="pagination__next"><?php next_posts_link( '<span class="pagination__next-link">next</span>' ); ?></div>
 
 				</div>
 
