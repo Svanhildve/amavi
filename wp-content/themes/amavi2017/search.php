6db851ca01aeb6d343f469696e4db408f6581ca2
Svanhild Egge <svanhildve@gmail.com>
2017-12-28 18:15:59 +0100

    search page if statements and some styling

--- wp-content/themes/amavi2017/search.php
+++ wp-content/themes/amavi2017/search.php
@@ -8,8 +8,36 @@
 
 				<div class="col-s-12">
 				
-					<h1 class="h1--search">Search results</h1>
-					<div class="search-intro">Your search for "<?php echo get_search_query(); ?>" returned <?php echo $total_results ?> results</div>
+					<h1 class="h1--search">
+
+						<? if ( have_posts() ) {
+						   echo 'Search results';
+						} 
+						else {
+							echo 'Nothing found';
+						}
+						?>
+							
+					</h1>
+
+					<div class="search-intro">
+
+						<? 
+						if ( have_posts() && ($total_results > 1)) {
+							echo 'Your search for "', get_search_query(), '" returned ', $total_results, ' results';
+						}
+
+						else if ( have_posts() && ($total_results == 1)) {
+							echo 'Your search for "', get_search_query(), '" returned ', $total_results, ' result';
+						} 
+
+						else {
+							echo 'There were no results matching your search. Try searching for something else.';
+						}
+
+						?>		
+
+					</div>
 
 					<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 						<label for="search-input" class="search-label visuallyhidden">Search for content</label>
@@ -45,21 +73,6 @@
 			
 <!-- LIMT INN -->
 
-
-				<div class="search-info col-s-12">
-
-					<h1>Nothing found</h1>
-
-					<div>There were no results matching your search. Try searching for something else.</div>
-
-					<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-						<label for="search-input" class="search-label visuallyhidden">Search for content</label>
-						<input type="text" name="s" class="" value="<?php the_search_query(); ?>">
-						<button type="submit" class="button button--search">Search</button>
-						<button type="submit" class="button button--ghost">Search</button>
-					</form> 	 
-
-				</div>
 
 			</div>
 
