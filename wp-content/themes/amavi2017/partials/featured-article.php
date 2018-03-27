0123b42fcad206b6616bc0bfc730507a2b67a931
Hannah Malcolm <kawaiihannah@gmail.com>
2018-03-27 20:33:19 +1000

    Add padding wrapper to align listings

--- wp-content/themes/amavi2017/partials/featured-article.php
+++ wp-content/themes/amavi2017/partials/featured-article.php
@@ -1,63 +1,63 @@
 
+<div class="col-s-12">
 
+	<div class="featured-article">
 
-<div class="featured-article">
-
-	<style>
-		@media screen and (max-width: 1023px) {
-			#featured-article__image--<?php echo get_post_thumbnail_id(); ?> {
-				background-image:url('<?php echo wp_get_attachment_image_url( get_post_thumbnail_id(), 'medium' ); ?>');
+		<style>
+			@media screen and (max-width: 1023px) {
+				#featured-article__image--<?php echo get_post_thumbnail_id(); ?> {
+					background-image:url('<?php echo wp_get_attachment_image_url( get_post_thumbnail_id(), 'medium' ); ?>');
+				}
 			}
-		}
-		@media screen and (min-width: 1024px) {
-			#featured-article__image--<?php echo get_post_thumbnail_id(); ?> {
-				background-image:url('<?php echo wp_get_attachment_image_url( get_post_thumbnail_id(), 'large' ); ?>');
+			@media screen and (min-width: 1024px) {
+				#featured-article__image--<?php echo get_post_thumbnail_id(); ?> {
+					background-image:url('<?php echo wp_get_attachment_image_url( get_post_thumbnail_id(), 'large' ); ?>');
 
-				;
+					;
+				}
 			}
-		}
-	</style>
+		</style>
 
-		<div class="featured-article__thumbnail-wrap col-s-12">
+			<div class="featured-article__thumbnail-wrap col-s-12">
 
-			<a href="<?php the_permalink(); ?>">
+				<a href="<?php the_permalink(); ?>">
 
-				<div class="featured-article__thumbnail-image" id="featured-article__image--<?php echo get_post_thumbnail_id(); ?>"></div>
+					<div class="featured-article__thumbnail-image" id="featured-article__image--<?php echo get_post_thumbnail_id(); ?>"></div>
 
-			</a>
-
-		</div>
-
-		<div class="featured-article__teaser row wrapper">
-
-			<div class="col-pm-offset-1 col-m-offset-3 col-s-12 col-pm-10 col-m-6 featured-article__teaser-body">
-
-				<div class="meta">
-
-					<div class="meta__date">
-
-						<?php echo get_the_date(); ?> /
-
-					</div>
-
-					<div class="meta__category">
-
-						<?php the_category(', '); ?>
-
-					</div>
-
-				</div>
-
-				<h2 class="h2 h2--featured-medium"><?php the_title(); ?></h2>
-
-				<div class="excerpt"><?php the_excerpt(); ?></div>
+				</a>
 
 			</div>
 
-		</div>
+			<div class="featured-article__teaser row wrapper">
+
+				<div class="col-pm-offset-1 col-m-offset-3 col-s-12 col-pm-10 col-m-6 featured-article__teaser-body">
+
+					<div class="meta">
+
+						<div class="meta__date">
+
+							<?php echo get_the_date(); ?> /
+
+						</div>
+
+						<div class="meta__category">
+
+							<?php the_category(', '); ?>
+
+						</div>
+
+					</div>
+
+					<h2 class="h2 h2--featured-medium"><?php the_title(); ?></h2>
+
+					<div class="excerpt"><?php the_excerpt(); ?></div>
+
+				</div>
+
+			</div>
 
 
+
+	</div>
 
 </div>
-
-	
